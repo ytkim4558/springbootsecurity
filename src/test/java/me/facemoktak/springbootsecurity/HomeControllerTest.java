019b05c39a029d9a9e0d97d6a3fdbcb1b4914f8c
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-04-04 20:55:48 +0900

    실패하던 테스트들 돌아가게끔 수정

--- src/test/java/me/facemoktak/springbootsecurity/HomeControllerTest.java
+++ src/test/java/me/facemoktak/springbootsecurity/HomeControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +23,7 @@ public class HomeControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     public void hello() throws Exception {
         mockMvc.perform(get("/hello"))
                 .andDo(print())
@@ -29,6 +32,14 @@ public class HomeControllerTest {
     }
 
     @Test
+    public void hello_without_user() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
     public void my() throws Exception {
         mockMvc.perform(get("/my"))
                 .andDo(print())
