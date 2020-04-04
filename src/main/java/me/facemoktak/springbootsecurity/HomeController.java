d45d850c872f799cd02250c81af3a42fcd0ba8f8
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-04-04 13:45:14 +0900

    연습용 컨트롤러및 테스트 추가

---
+++ src/main/java/me/facemoktak/springbootsecurity/HomeController.java
@@ -0,0 +1,18 @@
+package me.facemoktak.springbootsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/my")
+    public String my() {
+        return "my";
+    }
+}
